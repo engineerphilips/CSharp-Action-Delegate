bdf16f95dee748c7729c4998beb705c50e45843c
Philip Baba <engineerphilips@outlook.com>
2024-03-05 23:15:35 +0000

    Add project files.

---
+++ CSharp Action Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//This method calculates BMI
+static void ProcessBmi()
+{
+    double height = 175d;
+    double weight = 75d;
+    double bmi = 0d;
+    if (height > 0d && weight > 0d)
+    {
+        var h = height / 100d;
+
+        bmi = weight / h / h;
+    }
+
+    Console.WriteLine($"The BMI is : {bmi:N}.");
+}
+
+Action doWorkAction = ProcessBmi;
+doWorkAction();
