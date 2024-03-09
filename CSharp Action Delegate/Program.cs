0bbf278589518fe29253919c10ea28bfa105a189
Philip Baba <engineerphilips@outlook.com>
2024-03-09 12:02:29 +0000

    Func class added

--- CSharp Action Delegate/Program.cs
+++ CSharp Action Delegate/Program.cs
@@ -1,10 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//This method calculates BMI
-static void ProcessBmi()
+//static void ProcessBmi()
+//{
+//    double height = 175d; 
+//    double weight = 75d; 
+//    double bmi = 0d;
+//    if (height > 0d && weight > 0d)
+//    {
+//        var h = height / 100d; 
+//        bmi = weight / h / h;
+//    } 
+
+//    Console.WriteLine($"The BMI is : {bmi:N}.");
+//}
+
+//Action processBmiAction = new Action(ProcessBmi);
+
+//Action processBmiAction = ProcessBmi;
+//processBmiAction();
+
+//static void ProcessBmi(double height, double weight)
+//{
+//    double bmi = 0d;
+//    if (height > 0d && weight > 0d)
+//    {
+//        var h = height / 100d;
+
+//        bmi = weight / h / h;
+//    }
+
+//    Console.WriteLine($"The BMI is : {bmi:N}.");
+//}
+
+//Action<double, double> doWorkAction = new Action<double, double>(ProcessBmi);
+//doWorkAction(175d, 75d);
+
+Action<double, double> processBmiAction = (height, weight) =>
 {
-    double height = 175d;
-    double weight = 75d;
     double bmi = 0d;
     if (height > 0d && weight > 0d)
     {
@@ -14,7 +46,6 @@ static void ProcessBmi()
     }
 
     Console.WriteLine($"The BMI is : {bmi:N}.");
-}
+};
 
-Action doWorkAction = ProcessBmi;
-doWorkAction();
+processBmiAction(175d, 75d);
---
+++ CSharp Func Delegate/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//static double ProcessBmi()
+//{
+//    double height = 175d;
+//    double weight = 75d;
+//    double bmi = 0d;
+//    if (height > 0d && weight > 0d)
+//    {
+//        var h = height / 100d;
+
+//        bmi = weight / h / h;
+//    }
+
+//    return bmi;
+//}
+
+//Func<double> processBmiFunc = new Func<double>(ProcessBmi);
+//Console.WriteLine($"The BMI is : {processBmiFunc:N}.");
+
+//static double ProcessBmi(double height, double weight)
+//{
+//    double bmi = 0d;
+//    if (height > 0d && weight > 0d)
+//    {
+//        var h = height / 100d;
+
+//        bmi = weight / h / h;
+//    }
+
+//    return bmi;
+//}
+
+//Func<double, double, double> processBmiFunc = ProcessBmi;
+//Console.WriteLine($"The BMI is : {processBmiFunc(175d, 75d):N}.");
+
+Func<double, double, double> processBmiAction = (height, weight) =>
+{
+    double bmi = 0d;
+    if (height > 0d && weight > 0d)
+    {
+        var h = height / 100d;
+
+        bmi = weight / h / h;
+    }
+
+    return bmi;
+};
+
+Console.WriteLine($"The BMI is : {processBmiAction(175d, 75d):N}.");
