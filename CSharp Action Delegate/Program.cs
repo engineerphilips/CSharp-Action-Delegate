a8631b81f3bc8d46f474246c43e2705191342815
Philip Baba <engineerphilips@outlook.com>
2024-03-11 13:11:31 +0000

    With unit tests

--- CSharp Action Delegate/Program.cs
+++ CSharp Action Delegate/Program.cs
@@ -1,51 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSharp_Action_Delegate;
 
-//static void ProcessBmi()
-//{
-//    double height = 175d; 
-//    double weight = 75d; 
-//    double bmi = 0d;
-//    if (height > 0d && weight > 0d)
-//    {
-//        var h = height / 100d; 
-//        bmi = weight / h / h;
-//    } 
+//Non-Generic Action
+NonGenericActionDelegate nonGenericActionDelegate = new NonGenericActionDelegate();
+Action processBmiNonGenericAction = nonGenericActionDelegate.ProcessBmi;
 
-//    Console.WriteLine($"The BMI is : {bmi:N}.");
-//}
+processBmiNonGenericAction();
 
-//Action processBmiAction = new Action(ProcessBmi);
+// Generic Action
+GenericActionDelegate genericActionDelegate = new GenericActionDelegate();
+Action<double, double> processBmiGenericAction = genericActionDelegate.ProcessBmi;
 
-//Action processBmiAction = ProcessBmi;
-//processBmiAction();
+processBmiGenericAction(175d, 75d);
 
-//static void ProcessBmi(double height, double weight)
-//{
-//    double bmi = 0d;
-//    if (height > 0d && weight > 0d)
-//    {
-//        var h = height / 100d;
-
-//        bmi = weight / h / h;
-//    }
-
-//    Console.WriteLine($"The BMI is : {bmi:N}.");
-//}
-
-//Action<double, double> doWorkAction = new Action<double, double>(ProcessBmi);
-//doWorkAction(175d, 75d);
-
-Action<double, double> processBmiAction = (height, weight) =>
-{
-    double bmi = 0d;
-    if (height > 0d && weight > 0d)
-    {
-        var h = height / 100d;
-
-        bmi = weight / h / h;
-    }
-
-    Console.WriteLine($"The BMI is : {bmi:N}.");
-};
-
-processBmiAction(175d, 75d);
+//Lambda Expression Action
+LambdaActionDelegate lambdaActionDelegate = new LambdaActionDelegate();
+lambdaActionDelegate.BmiWithNonGenericAction();
+lambdaActionDelegate.BmiWithGenericAction(175d, 75d);
--- CSharp Func Delegate/Program.cs
+++ CSharp Func Delegate/Program.cs
@@ -1,50 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//static double ProcessBmi()
-//{
-//    double height = 175d;
-//    double weight = 75d;
-//    double bmi = 0d;
-//    if (height > 0d && weight > 0d)
-//    {
-//        var h = height / 100d;
+using CSharp_Func_Delegate;
 
-//        bmi = weight / h / h;
-//    }
+//Func delegate without input parameters
+FuncDelegateWithoutInputParameter funcDelegateWithoutInputParameter = new FuncDelegateWithoutInputParameter();
+Func<double> processfuncDelegateWithoutInputParameter = funcDelegateWithoutInputParameter.ProcessBmi;
+Console.WriteLine($"The BMI is : {processfuncDelegateWithoutInputParameter():N}.");
 
-//    return bmi;
-//}
+//Func delegate with input parameters
+FuncDelegateWithInputParameter funcDelegateWithInputParameter = new FuncDelegateWithInputParameter();
+Func<double, double, double> processFuncDelegateWithInputParameter = funcDelegateWithInputParameter.ProcessBmi;
+Console.WriteLine($"The BMI is : {processFuncDelegateWithInputParameter(175d, 75d):N}.");
 
-//Func<double> processBmiFunc = new Func<double>(ProcessBmi);
-//Console.WriteLine($"The BMI is : {processBmiFunc:N}.");
-
-//static double ProcessBmi(double height, double weight)
-//{
-//    double bmi = 0d;
-//    if (height > 0d && weight > 0d)
-//    {
-//        var h = height / 100d;
-
-//        bmi = weight / h / h;
-//    }
-
-//    return bmi;
-//}
-
-//Func<double, double, double> processBmiFunc = ProcessBmi;
-//Console.WriteLine($"The BMI is : {processBmiFunc(175d, 75d):N}.");
-
-Func<double, double, double> processBmiAction = (height, weight) =>
-{
-    double bmi = 0d;
-    if (height > 0d && weight > 0d)
-    {
-        var h = height / 100d;
-
-        bmi = weight / h / h;
-    }
-
-    return bmi;
-};
-
-Console.WriteLine($"The BMI is : {processBmiAction(175d, 75d):N}.");
+//Func delegate with lambda expressions
+FuncDelegateLambdaExpression funcDelegateLambdaExpression = new FuncDelegateLambdaExpression();
+Console.WriteLine($"The BMI is : {funcDelegateLambdaExpression.ProcessBmiWithInputParametersFunc(175d, 75d):N}.");
+Console.WriteLine($"The BMI is : {funcDelegateLambdaExpression.ProcessBmiWithNoInputParametersFunc():N}.");
